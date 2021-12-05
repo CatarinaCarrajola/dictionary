7818b181b4c3fd7bde6b09a047b35c4264959193
CatarinaCarrajola <catarinacarrajola+github@gmail.com>
2021-12-05 19:30:01 +0100

    Search Engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container"></div>
       <header className="App-header"></header>
-      Hello
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Catarina Carrajola</small>
+      </footer>
     </div>
   );
 }
