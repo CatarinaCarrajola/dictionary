6a268a9c475c4cf70844f38800831ee1af108239
CatarinaCarrajola <catarinacarrajola+github@gmail.com>
2021-12-12 13:57:41 +0100

    Styled the application

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import logo from "./logo.png";
 import "./App.css";
 import Dictionary from "./Dictionary";
 
@@ -5,9 +6,11 @@ function App() {
   return (
     <div className="App">
       <div className="container"></div>
-      <header className="App-header"></header>
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
       <main>
-        <Dictionary />
+        <Dictionary defaultKeyword="sunset" />
       </main>
       <footer className="App-footer">
         <small>Coded by Catarina Carrajola</small>
